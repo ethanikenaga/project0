16691661c9c996d9587092681210163ef13b1e51
Ethan Ikenaga <ethanikenaga@gmail.com>
2026-09-16 19:56:45 -0700

    feat: updateList updated

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,29 +1,34 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor",
-  },
-  {
-    name: "Mac",
-    job: "Bouncer",
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress",
-  },
-  {
-    name: "Dennis",
-    job: "Bartender",
-  },
-];
-
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+   });
+    return promise;
+  } 
+  useEffect(() => {
+  fetchUsers()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -32,8 +37,12 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-  setCharacters([...characters, person]);
+ function updateList(person) {
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
  return (
